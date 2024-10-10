0281473189113fbe6de58c97050b358f665d5dc2
René Martínez <hola@r3n0.com>
2024-10-10 15:58:14 -0500

    updates de arrays

--- sketch.js
+++ sketch.js
@@ -1,37 +1,50 @@
-let posX, posY;
-let velX, velY;
-let diam;
-let rad;
 let fonditoDeColorin;
+let pelotas = [];
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	fonditoDeColorin = color(random(100, 255), random(100), random(255));
-	rad = ceil(random(50, 100));
-	diam = rad * 2;
-	// console.log(diam);
-	posX = random(rad, width - rad);
-	posY = random(rad, height - rad);
-
-	velX = random(-10, 10);
-	velY = random(-10, 10);
+	for (let i = 0; i < 100; i++) {
+		let nuevaPelota = new Pelota();
+		pelotas.push(nuevaPelota);
+	}
 }
 
 function draw() {
 	background(fonditoDeColorin);
-	fill(255);
-	noStroke();
-
-	posX += velX;
-	posY += velY;
-
-	if (posX > width - rad || posX < rad) {
-		velX *= -1;
-		fonditoDeColorin = color(random(100, 255), random(100), random(255));
+	for (let i = 0; i < 100; i++) {
+		pelotas[i].update();
+		pelotas[i].display();
 	}
-	if (posY > height - rad || posY < rad) {
-		velY *= -1;
-		fonditoDeColorin = color(random(100, 255), random(100), random(255));
+}
+
+class Pelota {
+	// Plano de instrucciones para creae una pelota
+	constructor() {
+		this.rad = ceil(random(5, 30));
+		this.diam = this.rad * 2;
+		this.posX = random(this.rad, width - this.rad);
+		this.posY = random(this.rad, height - this.rad);
+		this.velX = random(-10, 10);
+		this.velY = random(-10, 10);
+		console.log('estoy viaaaaa!');
 	}
-	circle(posX, posY, diam);
+	// método update
+	update() {
+		this.posX += this.velX;
+		this.posY += this.velY;
+
+		if (this.posX > width - this.rad || this.posX < this.rad) {
+			this.velX *= -1;
+		}
+		if (this.posY > height - this.rad || this.posY < this.rad) {
+			this.velY *= -1;
+		}
+	}
+
+	display() {
+		fill(255);
+		noStroke();
+		circle(this.posX, this.posY, this.diam);
+	}
 }
